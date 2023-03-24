44f68576f2b622e164e996cb3baad06e83a456da
xulin <xl@gameduchy.com>
2023-03-24 15:10:40 +0800

    Layer onDetach something

--- Hazel/src/Hazel/LayerStack.cpp
+++ Hazel/src/Hazel/LayerStack.cpp
@@ -19,18 +19,21 @@ namespace Hazel
 	{
 		m_Layers.emplace(m_Layers.begin() + m_LayerInsertIndex, layer);
 		m_LayerInsertIndex++;
+		layer->OnAttach();
 	}
 
 	void LayerStack::PushOverlay(Layer* overlayer) 
 	{
 		m_Layers.emplace_back(overlayer);
+		overlayer->OnAttach();
 	}
 
 	void LayerStack::PopLayer(Layer* layer) 
 	{
-		auto it = std::find(m_Layers.begin(), m_Layers.end(), layer);
+		auto it = std::find(m_Layers.begin(), m_Layers.begin() + m_LayerInsertIndex, layer);
 		if (it != m_Layers.end())// find it successfully!
 		{
+			layer->OnDetach();
 			m_Layers.erase(it);
 			m_LayerInsertIndex--;
 		}
@@ -38,9 +41,13 @@ namespace Hazel
 
 	void LayerStack::PopOverlay(Layer* overlayer) 
 	{
-		auto it = std::find(m_Layers.begin(), m_Layers.end(), overlayer);
-		if (it != m_Layers.end()) 
+		auto it = std::find(m_Layers.begin(), m_Layers.begin() + m_LayerInsertIndex, overlayer);
+		if (it != m_Layers.end())
+		{
+			overlayer->OnDetach();
 			m_Layers.erase(it);
+		}
+			
 	}
 
 
