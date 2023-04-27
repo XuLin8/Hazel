864152dce7d23e117b112627392c41618680371c
xulin <xl@gameduchy.com>
2023-04-27 17:34:50 +0800

    Fixed GLM_ENABLE_EXPERIMENTAL not being defined in Log.h

--- Hazel/src/Hazel/Core/Log.h
+++ Hazel/src/Hazel/Core/Log.h
@@ -4,6 +4,9 @@
 
 #include "Hazel/Core/Base.h"
 
+#define GLM_ENABLE_EXPERIMENTAL
+#include "glm/gtx/string_cast.hpp"
+
 // This ignores all warnings raised inside External headers
 #pragma warning(push, 0)
 #include <spdlog/spdlog.h>
