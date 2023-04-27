0e252cfdb1acda50d19587da36d5d68a3ef03a85
xulin <xl@gameduchy.com>
2023-04-27 15:35:04 +0800

    New shader system,added Python scripts for retrieving dependencies,added uniform buffers,added command line args, and better premake scripts

--- premake5.lua
+++ premake5.lua
@@ -1,4 +1,5 @@
 include "./vendor/premake/premake_customization/solution_items.lua"
+include "Dependencies.lua"
 workspace "Hazel" --解决方案名称
     architecture "x86_64" --编译平台 只编64位--(x86,x86_64,ARM)
     startproject "Hazelnut"
@@ -20,16 +21,6 @@ workspace "Hazel" --解决方案名称
 --详细的所有支持的tokens 可参考 [https://github.com/premake/premake-core/wiki/Tokens]
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
---Include directories relative to root folder (solution diretory)
-IncludeDir = {}
-IncludeDir["GLFW"] = "%{wks.location}/Hazel/vendor/GLFW/include"
-IncludeDir["Glad"] = "%{wks.location}/Hazel/vendor/Glad/include"
-IncludeDir["ImGui"] = "%{wks.location}/Hazel/vendor/imgui"
-IncludeDir["glm"] = "%{wks.location}/Hazel/vendor/glm"
-IncludeDir["stb_image"] = "%{wks.location}/Hazel/vendor/stb_image"
-IncludeDir["entt"] = "%{wks.location}/Hazel/vendor/entt/include"
-IncludeDir["yaml_cpp"] = "%{wks.location}/Hazel/vendor/yaml-cpp/include"
-IncludeDir["ImGuizmo"] = "%{wks.location}/Hazel/vendor/ImGuizmo"
 
 group "Dependencies"
 	include "vendor/premake"
