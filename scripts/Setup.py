c194c1a7f480314043b4bb78edaf71fac6f9af04
xulin <xl@gameduchy.com>
2023-04-28 17:28:52 +0800

    Changed required Vulkan SDK version to specific 1.3 and removed local debug libs

--- scripts/Setup.py
+++ scripts/Setup.py
@@ -1,3 +1,4 @@
+
 import os
 import subprocess
 import platform
@@ -7,16 +8,15 @@ from SetupPython import PythonConfiguration as PythonRequirements
 # Make sure everything we need for the setup is installed
 PythonRequirements.Validate()
 
-#Setup script now updates git submodules
-print("\nUpdating submodules...")
-subprocess.call(["git", "submodule", "update", "--init", "--recursive"])
-
 from SetupPremake import PremakeConfiguration as PremakeRequirements
 from SetupVulkan import VulkanConfiguration as VulkanRequirements
 os.chdir('./../') # Change from devtools/scripts directory to root
 
 premakeInstalled = PremakeRequirements.Validate()
 VulkanRequirements.Validate()
+
+print("\nUpdating submodules...")
+subprocess.call(["git", "submodule", "update", "--init", "--recursive"])
 
 if (premakeInstalled):
     if platform.system() == "Windows":
